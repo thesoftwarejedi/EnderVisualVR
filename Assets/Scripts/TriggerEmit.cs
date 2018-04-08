afb857ef0ce1038df324e855986553682c7dd909
Dana Hanna <dana.hanna@gmail.com>
2018-04-08 15:31:02 -0400

    about to add vrtk

--- Assets/Scripts/TriggerEmit.cs
+++ Assets/Scripts/TriggerEmit.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class TriggerEmit : MonoBehaviour {
 
     private SteamVR_TrackedController _controller;
+    private ParticleSystem _ps;
 
     // Use this for initialization
     void Start () {
+        _ps = GetComponentInChildren<ParticleSystem>();
         _controller = GetComponent<SteamVR_TrackedController>();
         _controller.TriggerClicked += _controller_TriggerClicked;
 	}
 
     private void _controller_TriggerClicked(object sender, ClickedEventArgs e)
     {
-        GetComponentInChildren<CircleWaveform>().Blast(false);
+        var em = _ps.emission;
+        em.rateOverDistanceMultiplier = e.padX;
     }
 
 }
